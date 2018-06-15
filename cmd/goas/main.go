92025643487ae035d0695240eefe184630c08329
呂浩宇 <y26704325@gmail.com>
2018-06-15 17:50:34 +0800

    paging.Hits

--- cmd/goas/main.go
+++ cmd/goas/main.go
@@ -33,6 +33,7 @@ func action(c *cli.Context) error {
 	// fmt.Println(c.GlobalString("output"))
 	return g.CreateOASFile(c.GlobalString("output"))
 
+	//Harry
 	// return g.CreateOASFile(filepath.Dir(os.Args[0]) + "\\index.json") //Harry: Current running path
 	// fmt.Println(filepath.Dir())
 	// return g
@@ -59,6 +60,7 @@ func main() {
 		log.Fatal(err)
 	}
 
+	//Harry
 	// err := action()
 	// if err != nil {
 	// 	log.Fatal(err)
